686100b8ae68c9660f3b72f0e0d72cf7a0f41e87
ryad <r.zenine@gmail.com>
2014-03-09 10:51:12 +0100

    simplifacatio de mpi.Dummy

--- mpi/dummy.go
+++ mpi/dummy.go
@@ -1,63 +1,39 @@
 package mpi
 
-import "container/list"
-
-type Dummy struct {
-  // represent the queues
-  queues map[string]*list.List
-  // represent the capacity of the queues when each queue fill it's capacity
-  // the older messages are removed
-  capacity int
-}
+type Dummy map[string]Versionable
 
 func (m *Dummy) Queues() []string {
-  names := make([]string, 0, len(m.queues))
-  for key, _ := range m.queues {
+  names := make([]string, 0, len(m.))
+  for key, _ := range m {
     names = append(names, key)
   }
   return names
 }
 
 func (m *Dummy) Register(name string) bool {
-  _, ok := m.queues[name]
+  _, ok := m[name]
   if !ok {
-    m.queues[name] = new(list.List)
+    m[name] = nil
     return true
   }
   return false
 }
 
 func (m *Dummy) Write(queue string, data Versionable) {
-  if _, ok := m.queues[queue]; ok {
-    m.queues[queue].PushFront(data)
-  } else {
-    ls := list.List{}
-    ls.PushFront(data)
-    if ls.Len() > m.capacity {
-      ls.Remove(ls.Back())
-    }
-    m.queues[queue] = &ls
-  }
+  m[queue] = data
 }
 
 func (m *Dummy) ReadFirst(queue string) (data Versionable) {
-  v, _ := m.queues[queue].Front().Value.(Versionable)
-  return v
+  return m[queue]
 }
 
 func (m *Dummy) ReadFirstAll() (data map[string]Versionable) {
-  data = make(map[string]Versionable)
-  for key, value := range m.queues {
-    v, _ := value.Front().Value.(Versionable)
-    data[key] = v
-  }
-  return data
+  return m
 }
 
 func (m *Dummy) ReadStates(versions map[string]int) map[string]Versionable {
-  tmp := m.ReadFirstAll()
   data := make(map[string]Versionable)
-  for key, v := range tmp {
+  for key, v := range m {
     if v.Version() > versions[key] {
       data[key] = v
     }
@@ -66,5 +42,5 @@ func (m *Dummy) ReadStates(versions map[string]int) map[string]Versionable {
 }
 
 func NewDummy(capacity int) (queue *Dummy) {
-  return &Dummy{make(map[string]*list.List), capacity}
+  return &Dummy(make(map[string]Versionable))
 }
