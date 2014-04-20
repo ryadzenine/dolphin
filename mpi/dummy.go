883fb7ccb4ed21a75f091bd38fadc822f42044f3
ryad <r.zenine@gmail.com>
2014-04-20 10:18:08 +0200

    Go fmt on the code

--- mpi/dummy.go
+++ mpi/dummy.go
@@ -3,44 +3,44 @@ package mpi
 type Dummy map[string]Versionable
 
 func (m Dummy) Queues() []string {
-  names := make([]string, 0, len(m))
-  for key, _ := range m {
-    names = append(names, key)
-  }
-  return names
+	names := make([]string, 0, len(m))
+	for key, _ := range m {
+		names = append(names, key)
+	}
+	return names
 }
 
 func (m Dummy) Register(name string) bool {
-  _, ok := m[name]
-  if !ok {
-    m[name] = nil
-    return true
-  }
-  return false
+	_, ok := m[name]
+	if !ok {
+		m[name] = nil
+		return true
+	}
+	return false
 }
 
 func (m Dummy) Write(queue string, data Versionable) {
-  m[queue] = data
+	m[queue] = data
 }
 
 func (m Dummy) ReadFirst(queue string) (data Versionable) {
-  return m[queue]
+	return m[queue]
 }
 
 func (m Dummy) ReadFirstAll() (data map[string]Versionable) {
-  return m
+	return m
 }
 
 func (m Dummy) ReadStates(versions map[string]int) map[string]Versionable {
-  data := make(map[string]Versionable)
-  for key, v := range m {
-    if v.Version() > versions[key] {
-      data[key] = v
-    }
-  }
-  return data
+	data := make(map[string]Versionable)
+	for key, v := range m {
+		if v.Version() > versions[key] {
+			data[key] = v
+		}
+	}
+	return data
 }
 
 func NewDummy() (queue Dummy) {
-  return Dummy(make(map[string]Versionable))
+	return Dummy(make(map[string]Versionable))
 }
